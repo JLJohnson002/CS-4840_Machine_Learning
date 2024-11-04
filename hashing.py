1088f8c4f743810fa5f0e84165ce39363235f7d8
Jimmy Johnson <89602279+JLJohnson002@users.noreply.github.com>
2024-11-04 18:15:15 -0500

    update

--- hashing.py
+++ hashing.py
@@ -1,9 +1,48 @@
 import dhash
 from PIL import Image
 
-image = Image.open('Death_star1.png')
+image1 = Image.open("DeathStar1.png")
+row, col = dhash.dhash_row_col(image1)
+hash1 = dhash.format_hex(row, col)
+print(dhash.format_hex(row, col))
+
+image2 = Image.open("DeathStar1.1.png")
+row, col = dhash.dhash_row_col(image2)
+hash2 = dhash.format_hex(row, col)
+print(dhash.format_hex(row, col))
+
+image3 = Image.open("DeathStar1.2.png")
+row, col = dhash.dhash_row_col(image3)
+# int3 = dhash.format_int(row, col)
+# print("Int is "+int3)
+hash3 = dhash.format_hex(row, col)
+print(dhash.format_hex(row, col))
+
+print("\nRest of them Below\n")
+
+image = Image.open("DeathStar2.jpg")
 row, col = dhash.dhash_row_col(image)
 print(dhash.format_hex(row, col))
+image = Image.open("DeathStar3.jpeg")
+row, col = dhash.dhash_row_col(image)
+print(dhash.format_hex(row, col))
+image = Image.open("DeathStar4.jpg")
+row, col = dhash.dhash_row_col(image)
+print(dhash.format_hex(row, col))
+image = Image.open("DeathStar5.png")
+row, col = dhash.dhash_row_col(image)
+print(dhash.format_hex(row, col))
+image = Image.open("DeathStar6.png")
+row, col = dhash.dhash_row_col(image)
+print(dhash.format_hex(row, col))
+image = Image.open("DeathStar7.png")
+row, col = dhash.dhash_row_col(image)
+print(dhash.format_hex(row, col))
+image = Image.open("DeathStar8.png")
+row, col = dhash.dhash_row_col(image)
+
+
+dhash.get_num_bits_different(dhash.dhash_row_colrg(image1), dhash.dhash_row_col(image2))
 
 # Traditional hashing 2 pixles different in each photo
 # 2bd793518de4cdff1ab88b43c8dfdc192b2a402597ff8888db5a685cc1ce69d8 - Death star 1
