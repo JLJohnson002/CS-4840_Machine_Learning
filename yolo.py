a985f9f786fd3e89e6753a57d074844a0592d5aa
Jimmy Johnson <89602279+JLJohnson002@users.noreply.github.com>
2024-11-21 16:38:47 -0500

    update

--- yolo.py
+++ yolo.py
@@ -1,54 +1,32 @@
-import yolo
-import cv2
-import numpy as np
+import torch
+from PIL import Image
+import os
 
-# Load YOLO
-net = cv2.dnn.readNet("yolov4.weights", "yolov4.cfg")
-layer_names = net.getLayerNames()
-output_layers = [layer_names[i[0] - 1] for i in net.getUnconnectedOutLayers()]
 
-image = cv2.imread("Images\DeathStarGive35.png")
-height, width, channels = image.shape
+folder_path = r"Images"
+itter = 0
+for item in os.listdir(folder_path):
 
-# Prepare the image for detection
-blob = cv2.dnn.blobFromImage(image, 0.00392, (416, 416), (0, 0, 0), True, crop=False)
-net.setInput(blob)
-outs = net.forward(output_layers)
+    model = torch.hub.load('ultralytics/yolov5', 'yolov5s', pretrained=True)
 
-# Process the detections
-class_ids = []
-confidences = []
-boxes = []
+    # Images
+    item_path = os.path.join(folder_path, item)
+    # img = "Images\DeathStar3.jpeg"  # or file, Path, PIL, OpenCV, numpy, list
 
-for out in outs:
-    for detection in out:
-        scores = detection[5:]
-        class_id = np.argmax(scores)
-        confidence = scores[class_id]
+    # Inference
+    model.conf = 0.2
+    results = model(item_path, size=320)
 
-        # Filter weak detections based on confidence
-        if confidence > 0.5:
-            center_x = int(detection[0] * width)
-            center_y = int(detection[1] * height)
-            w = int(detection[2] * width)
-            h = int(detection[3] * height)
+    # Results
+    # results.show()  # or .show(), .save(), .crop(), .pandas(), etc.
 
-            # Draw bounding box
-            x = int(center_x - w / 2)
-            y = int(center_y - h / 2)
-            boxes.append([x, y, w, h])
-            confidences.append(float(confidence))
-            class_ids.append(class_id)
 
-# Apply Non-Maximum Suppression (NMS) to remove overlapping boxes
-indexes = cv2.dnn.NMSBoxes(boxes, confidences, 0.5, 0.4)
+    # Load the original image
+    ori_img = Image.open(item_path)
+    for i, (*box, conf, cls) in enumerate(results.xyxy[0]):  # xyxy format
+        x1, y1, x2, y2 = map(int, box[:4])  # Extract bounding box
+        cropped_img = ori_img.crop((x1, y1, x2, y2))  # Crop region
+        cropped_img.convert("RGB").save(f"{item[:-4]}_cropped_{i}.png")  # Save the cropped image
+    itter += 1
 
-for i in range(len(boxes)):
-    if i in indexes:
-        x, y, w, h = boxes[i]
-        cv2.rectangle(image, (x, y), (x + w, y + h), (0, 255, 0), 2)
-
-# Display the result
-cv2.imshow("Image", image)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+print ("\nFINISHED\n")
