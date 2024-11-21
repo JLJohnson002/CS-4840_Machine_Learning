2cee1191d16e4dbbfcf5f7f3e04fed76494785ea
Jimmy Johnson <89602279+JLJohnson002@users.noreply.github.com>
2024-11-20 19:24:33 -0500

    added workint yolo

---
+++ yolo.py
@@ -0,0 +1,54 @@
+import yolo
+import cv2
+import numpy as np
+
+# Load YOLO
+net = cv2.dnn.readNet("yolov4.weights", "yolov4.cfg")
+layer_names = net.getLayerNames()
+output_layers = [layer_names[i[0] - 1] for i in net.getUnconnectedOutLayers()]
+
+image = cv2.imread("Images\DeathStarGive35.png")
+height, width, channels = image.shape
+
+# Prepare the image for detection
+blob = cv2.dnn.blobFromImage(image, 0.00392, (416, 416), (0, 0, 0), True, crop=False)
+net.setInput(blob)
+outs = net.forward(output_layers)
+
+# Process the detections
+class_ids = []
+confidences = []
+boxes = []
+
+for out in outs:
+    for detection in out:
+        scores = detection[5:]
+        class_id = np.argmax(scores)
+        confidence = scores[class_id]
+
+        # Filter weak detections based on confidence
+        if confidence > 0.5:
+            center_x = int(detection[0] * width)
+            center_y = int(detection[1] * height)
+            w = int(detection[2] * width)
+            h = int(detection[3] * height)
+
+            # Draw bounding box
+            x = int(center_x - w / 2)
+            y = int(center_y - h / 2)
+            boxes.append([x, y, w, h])
+            confidences.append(float(confidence))
+            class_ids.append(class_id)
+
+# Apply Non-Maximum Suppression (NMS) to remove overlapping boxes
+indexes = cv2.dnn.NMSBoxes(boxes, confidences, 0.5, 0.4)
+
+for i in range(len(boxes)):
+    if i in indexes:
+        x, y, w, h = boxes[i]
+        cv2.rectangle(image, (x, y), (x + w, y + h), (0, 255, 0), 2)
+
+# Display the result
+cv2.imshow("Image", image)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
---
+++ yoloworking.py
@@ -0,0 +1,28 @@
+import torch
+from PIL import Image
+import os
+
+
+folder_path = r"Images"
+for item in os.listdir(folder_path):
+    model = torch.hub.load('ultralytics/yolov5', 'yolov5s', pretrained=True)
+
+    # Images
+    item_path = os.path.join(folder_path, item)
+    # img = "Images\DeathStar3.jpeg"  # or file, Path, PIL, OpenCV, numpy, list
+
+    # Inference
+    model.conf = 0.2
+    results = model(item_path, size=320)
+
+    # Results
+    results.show()  # or .show(), .save(), .crop(), .pandas(), etc.
+
+
+    # Load the original image
+    ori_img = Image.open(item_path)
+
+    for i, (*box, conf, cls) in enumerate(results.xyxy[0]):  # xyxy format
+        x1, y1, x2, y2 = map(int, box[:4])  # Extract bounding box
+        cropped_img = ori_img.crop((x1, y1, x2, y2))  # Crop region
+        cropped_img.convert("RGB").save(f"cropped_{i}.jpg")  # Save the cropped image
