46ffe78d7000a95dd547f19f12bae62f4aaf3bde
Jimmy Johnson <89602279+JLJohnson002@users.noreply.github.com>
2024-11-25 09:38:49 -0500

    Revert "upate"
    This reverts commit 15fcec672171d50d37910e7841f2967f35f1664c.

--- yolo.py
+++ yolo.py
@@ -16,10 +16,7 @@ for item in os.listdir(folder_path):
 
     # Inference
     model.conf = 0.2
-<<<<<<< HEAD
 
-=======
->>>>>>> parent of 230d9eb (all images processed)
     results = model(item_path, size=320)
 
     # Results
